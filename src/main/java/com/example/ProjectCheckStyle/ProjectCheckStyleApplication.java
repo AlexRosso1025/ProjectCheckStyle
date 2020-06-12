56d6dcbf0fe830fd4a89846532ed5fa7ff115170
AlexRosso1025 <31864275+AlexRosso1025@users.noreply.github.com>
2020-06-12 16:31:05 -0500

    first commit

---
+++ src/main/java/com/example/ProjectCheckStyle/ProjectCheckStyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectCheckStyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCheckStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCheckStyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectCheckStyle/ProjectCheckStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectCheckStyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCheckStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
